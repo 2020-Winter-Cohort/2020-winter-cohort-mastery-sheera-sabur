c0cb9508a9c98a01a424bb1f0853c94d587dd382
basheeraa <basheera_a@hotmail.com>
2021-02-20 22:43:34 -0500

    clean up unused code

--- src/main/java/com/survivingcodingbootcamp/blog/controller/HashtagController.java
+++ src/main/java/com/survivingcodingbootcamp/blog/controller/HashtagController.java
@@ -7,10 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-import javax.websocket.server.PathParam;
-import java.util.HashSet;
-
 @Controller
 @RequestMapping("/hashtags")
 public class HashtagController {
